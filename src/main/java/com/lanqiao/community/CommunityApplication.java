7ca7155f670ac29b4b5e8e40b3fb96a5d1e34c90
Deep2018530 <1184858443@qq.com>
2019-06-10 23:49:34 +0800

    init repo

---
+++ src/main/java/com/lanqiao/community/CommunityApplication.java
@@ -0,0 +1,16 @@
+package com.lanqiao.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author DeepSleeping
+ */
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
