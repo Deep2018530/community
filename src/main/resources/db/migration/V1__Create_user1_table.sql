d49d7b52617eb3170d4f70e3e753cb897d762c45
deepz <1184858443@qq.com>
2019-06-12 17:30:19 +0800

    nothing

---
+++ src/main/resources/db/migration/V1__Create_user1_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `user1`
+(
+    `id`           int(11) NOT NULL AUTO_INCREMENT,
+    `name`         varchar(100) DEFAULT NULL,
+    `account_id`   varchar(50)  DEFAULT NULL,
+    `token`        char(36)     DEFAULT NULL,
+    `gmt_create`   bigint(20)   DEFAULT NULL,
+    `gmt_modified` bigint(20)   DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 11
+  DEFAULT CHARSET = utf8;
