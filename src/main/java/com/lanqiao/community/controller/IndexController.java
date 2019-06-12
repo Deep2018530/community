2f812fc284b6673d19ea2b089e4488f79ea42c8a
deepz <1184858443@qq.com>
2019-06-12 15:58:02 +0800

    login cookie

--- src/main/java/com/lanqiao/community/controller/IndexController.java
+++ src/main/java/com/lanqiao/community/controller/IndexController.java
@@ -1,7 +1,14 @@
 package com.lanqiao.community.controller;
 
+import com.lanqiao.community.mapper.UserMapper;
+import com.lanqiao.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author DeepSleeping
@@ -11,8 +18,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String hello() {
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (!ObjectUtils.isEmpty(user)) {
+                    request.getSession().setAttribute("githubUser", user);
+                }
+                break;
+            }
+        }
+
+
         return "index";
     }
 }
--- src/main/java/com/lanqiao/community/mapper/UserMapper.java
+++ src/main/java/com/lanqiao/community/mapper/UserMapper.java
@@ -4,6 +4,8 @@ package com.lanqiao.community.mapper;
 import com.lanqiao.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * @author DeepSleeping
@@ -15,4 +17,7 @@ public interface UserMapper {
 
     @Insert("insert into user_1(name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user_1 where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
